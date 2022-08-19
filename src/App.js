55a26563f72c4fa8add75970b416fcc39b498344
bootsrap <adhiltajudeen@gmail.com>
2022-08-19 10:24:10 +0530

    router using button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./Component/About";
+import Profile from "./Component/Profile";
+import{useState} from 'react'
+import {Route,Routes,Link,useNavigate} from 'react-router-dom'
+
 
 function App() {
+  const [state,setState]=useState('')
+  const navigate=useNavigate()
+
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <button onClick={()=>navigate('/a')} >About</button>
+    <button onClick={()=>navigate('/p')} >profile</button>
+    
+   
+  
+  
+    
+      
+      <Routes>
+      <Route  exact path='/a' element={<About/>} />
+      <Route  exact path='/p' element={<Profile/>} />
+      </Routes>
+  
+    
+  
+   </div>
   );
 }
-
 export default App;
